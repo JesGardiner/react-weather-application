301c5d9fb3c7219f2ac4d9deb4975299e3db9d08
JesGardiner <jes.gardiner21@gmail.com>
2024-08-12 12:00:58 +0100

    Added a header and footer

--- src/App.js
+++ src/App.js
@@ -4,9 +4,22 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
-      <header>
-        <h1>Weather App</h1>
-      </header>
+      <h1>Weather App</h1>
+      <footer>
+        This project was coded by{" "}
+        <a href="https://github.com/JesGardiner" target="_blank">
+          {" "}
+          Jessica Gardiner
+        </a>{" "}
+        and is{" "}
+        <a
+          href="https://github.com/JesGardiner/react-weather-application"
+          target="_blank"
+        >
+          open-soured on GitHub
+        </a>{" "}
+        & hosted on Netlify.
+      </footer>
     </div>
   );
 }
